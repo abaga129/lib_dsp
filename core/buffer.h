04aaacfb6a378acba5f2246c723574b99944bac7
Ethan Reker <ethanepr@hotmail.com>
2016-05-25 21:31:26 -0500

    Update buffer.h

--- core/buffer.h
+++ core/buffer.h
@@ -1,16 +1,23 @@
-#ifndef buffer.h
-#define buffer.h
+#ifndef BUFFER_H
+#define BUFFER_H
 
 namespace dsp
 {
 
 class Buffer{
 public:
-
+  Buffer(size_t size);
 private:
-  std::vector<float> vec;
+  float *data;
+  int writeIndex;
+  int readIndex;
 };
 
+Buffer(size_t size){
+  data = new float(size);
+  writeIndex = 0;
+  readIndex = 1;
 }
 
+}//end namespace dsp
 #endif
