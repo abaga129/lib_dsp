1964eb6afbd60a51d9ce030e583439ccd4a8e92e
Ethan Reker <ethanepr@hotmail.com>
2016-05-25 21:58:28 -0500

    Update buffer.h

--- core/buffer.h
+++ core/buffer.h
@@ -7,16 +7,21 @@ namespace dsp
 class Buffer{
 public:
   Buffer(size_t size);
+  float getSample(size_t index);
 private:
   float *data;
   int writeIndex;
   int readIndex;
 };
 
-Buffer(size_t size){
+Buffer::Buffer(size_t size){
   data = new float(size);
   writeIndex = 0;
   readIndex = 1;
+}
+
+float Buffer::getSample(size_t index){
+  return data[index];
 }
 
 }//end namespace dsp
