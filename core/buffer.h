0d09712be907de88204e1f63ad769a447792cdd5
Ethan Reker <ethanepr@hotmail.com>
2016-05-25 22:06:21 -0500

    add getWritePointer()

--- core/buffer.h
+++ core/buffer.h
@@ -8,16 +8,24 @@ class Buffer{
 public:
   Buffer(size_t size);
   float getSample(size_t index);
+  float* getWritePointer();
 private:
   float *data;
   int writeIndex;
   int readIndex;
+  size_t numSamples;
 };
 
 Buffer::Buffer(size_t size){
   data = new float(size);
   writeIndex = 0;
   readIndex = 1;
+  numSamples = size;
+}
+
+float* Buffer::getWritePointer(){
+  float *ptr = data;
+  return ptr;
 }
 
 float Buffer::getSample(size_t index){
