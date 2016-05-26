4299cd056b8e81565357431a47369515a04a190e
Ethan Reker <ethanepr@hotmail.com>
2016-05-25 22:59:33 -0500

    Update dsp_functions.h

--- include/dsp_functions.h
+++ include/dsp_functions.h
@@ -13,5 +13,13 @@ float decibelToFloat(float value){
   return pow(10, (value/20));
 }
 
+float msToSamples(float ms, float sampleRate){
+  return ms * (sampleRate / 1000);
+}
+
+float samplesToMs(float samples, float sampleRate){
+  return samples/(sampleRate/1000);
+}
+
 }//end namespace
 #endif
