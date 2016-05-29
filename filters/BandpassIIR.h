9a598009e18ff4893165dcaeb79b14d192b39f8d
Ethan Reker <ethanepr@hotmail.com>
2016-05-29 16:12:19 -0500

    Create BandpassIIR.h

---
+++ filters/BandpassIIR.h
@@ -0,0 +1,13 @@
+/***********************************
+* Created By Ethan Reker
+* On May 29, 2016
+* Purpose: A class for implementing a simple bandpass filter.
+***********************************/
+
+#ifndef BANDPASSIIR_H
+#define BANDPASSIIR_H
+
+#include 
+
+
+#endif
