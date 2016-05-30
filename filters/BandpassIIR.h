1c8c4351d98ece8e8ba119edd16285998935bc77
Ethan Reker <ethanepr@hotmail.com>
2016-05-29 22:09:54 -0500

    Update BandpassIIR.h

--- filters/BandpassIIR.h
+++ filters/BandpassIIR.h
@@ -1,8 +1,10 @@
-/***********************************
+/**********************************************
 * Created By Ethan Reker
 * On May 29, 2016
-* Purpose: A class for implementing a simple bandpass filter.
-***********************************/
+* Purpose: An adapter class for implementing a 
+*          bandpass filter using a highpass and
+*          lowpass in series.
+**********************************************/
 
 #ifndef BANDPASSIIR_H
 #define BANDPASSIIR_H
@@ -13,11 +15,32 @@
 namespace dsp
 {
 
-class BandpassIIR{
-  BandpassIIR();
-  float getNextSample();
+class BandpassIIR : public BiQuadFilter {
+public:
+  BandpassIIR(int numChannels, float fc, float fs){
+    lowpass = new LowpassIIR(numChannels, fc, fs);
+    highpass = new HighpassIIR(numChannels, fc, fs);
+  }
+  float nextSample(float sample, int channel){
+    return highpass->nextSample(lowpass->nextSample(sample, channel), channel);
+  }
+  void update(float fc, float fs){
+    lowpass->update(fc, fs);
+    highpass->update(fc, fs);
+  }
   
-}
+  void setSampleRate(double sampleRate) {
+    lowpass->setSampleRate(sampleRate);
+    highpass->getSampleRate(sampleRate);
+  }
+  double getSampleRate() { return fs; }{
+    return lowpass->getSampleRate();
+  } 
+
+private:
+  LowpassIIR *lowpass;
+  HighpassIIR *highpass;
+};
   
 }//end namespace dsp
 #endif
